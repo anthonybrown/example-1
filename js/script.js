4e7655f1d425a7bfc6ab007842395c2662c388a1
Tony Brown <tony.brown.357@gmail.com>
2014-06-06 17:35:43 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,1 @@
+var Site={init:function(){this.tabs()},tabs:function(){var a=$("#sidebar > .tabs"),b=a.find("> ul"),c=a.prev("#tabHeadings");c.delegate("li","click",function(a){var c=$(this),d;c.siblings().removeClass("selected").end().addClass("selected");d=c.children("a").attr("href");b.hide().filter(d).fadeIn(500);a.preventDefault()})}};Site.init()
